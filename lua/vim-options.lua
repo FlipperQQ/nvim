81ebcacb606296893a295c0c451ed4c893e5470a
flipper <flipper@flip.com>
2025-10-08 22:24:25 +0900

    update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,4 @@
+vim.scriptencoding = "utf-8"
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -7,19 +8,27 @@ vim.cmd("set autoindent")
 vim.cmd("set smartindent")
 vim.cmd("set number")
 vim.cmd("inoremap jk <Esc>")
+vim.cmd("vnoremap jk <Esc>")
 vim.g.mapleader = " "
--- vim.g.mapleader = ","
 vim.g.background = "light"
 vim.opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = true
+vim.opt.spell = true
+-- Search
+vim.opt.wrapscan = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<leader>n", ":nohlsearch <CR>")
 -- Navigate vim panes better
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
---vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.cmd("set nohlsearch")
 -- buffer
 vim.keymap.set("n", "<leader>h", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<leader>l", "<cmd>bnext<CR>")
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>")
 vim.keymap.set("n", "<leader>be", "<cmd>%bd|e#|bd#<CR>")
+
